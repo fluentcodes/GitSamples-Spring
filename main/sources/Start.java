182c0269e7cf84a83f45dd3d982e6720e2653350
WerDiw <werner.diwischek@witt-gruppe.eu>
2022-03-18 09:37:12 +0100

    java-springbot-empty-direct: before branch to java-springboot-CommandLineRunner

--- main/sources/Start.java
+++ main/sources/Start.java
@@ -4,11 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
 
-/**
- * Created by werner.diwischek on 11.12.17.
- */
-
-//https://stackoverflow.com/questions/24661289/spring-boot-not-serving-static-content
 @SpringBootApplication
 public class Start {
     public static void main(String[] args) {
